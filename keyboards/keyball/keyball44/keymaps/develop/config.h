f5cd783f8ecee7bfde29295708612d1e74606cc1
Sergiy Pavlyuk <s.pavlyuk@gmail.com>
2025-01-28 00:53:15 +0100

    saving progress

--- keyboards/keyball/keyball44/keymaps/develop/config.h
+++ keyboards/keyball/keyball44/keymaps/develop/config.h
@@ -31,3 +31,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #    define RGBLIGHT_EFFECT_ALTERNATING
 #    define RGBLIGHT_EFFECT_TWINKLE
 #endif
+
+#define TAP_CODE_DELAY 5
+
+#define POINTING_DEVICE_AUTO_MOUSE_ENABLE
+#define AUTO_MOUSE_DEFAULT_LAYER 1
--- keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -24,20 +24,44 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for development
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS  ,
-    KC_LCTL  , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_ENT   ,
-                    KC_LGUI  , KC_LALT  , SCRL_MO  ,       KC_SPC   , LT(1,KC_TAB),           LT(1,KC_BSPC), KC_ENT   ,      SCRL_MO  , KC_RALT  , KC_RGUI
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     , KC_ESC   ,                                        _______  , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     , KC_TAB  ,                                         _______  , KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , 
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LSFT  ,                                        _______  , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , 
+                    SCRL_MO  , KC_LALT  ,  KC_LCTL,    LT(1,KC_SPC)   , LT(2,_______),           KC_BSPC, LT(3,KC_ENT)   ,      _______  , _______  , KC_LGUI
   ),
 
   [1] = LAYOUT_universal(
+    S(KC_8), KC_7     , KC_8    , KC_9     , S(KC_EQL)  , KC_GRV ,                                           S(KC_9)  , S(KC_0)   , KC_LBRC, KC_RBRC, S(KC_LBRC)  , S(KC_LBRC)  ,
+    KC_SLSH, KC_4     , KC_5    , KC_6     , KC_MINS  , S(KC_GRV)  ,                                         S(KC_1)  , S(KC_2)   , S(KC_3),  S(KC_4)  , S(KC_5)  , S(KC_6)  ,
+    _______, KC_1     , KC_2    , KC_3     , KC_EQL, _______  ,                                              S(KC_7)  , S(KC_UNDS), KC_BSLS,S(KC_BSLS),KC_SLSH    , _______,
+                  KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   _______   , _______  , _______       , _______  , _______
+  ),
+
+  [2] = LAYOUT_universal(
+    KC_F1    , KC_F2    , KC_F3   , KC_F4    , KC_F5    , KC_F6    ,                                         KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
+    SSNP_VRT ,  KC_PGUP , KC_BTN1  , KC_UP   , KC_BTN2  , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_HOME  ,
+    SSNP_HOR ,  KC_PGDN , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_BSPC  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , KC_END  ,
+                  SSNP_FRE , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+  ),
+
+  [3] = LAYOUT_universal(
     RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
     RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
                     QK_BOOT  , KBC_RST  , _______  ,       _______  , _______     ,               _______  , _______  ,      _______  , KBC_RST  , QK_BOOT
   ),
+
+
+  
 };
 // clang-format on
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    // Auto enable scroll mode when the highest layer is 3
+    keyball_set_scroll_mode(get_highest_layer(state) == 3);
+    return state;
+}
+
 
 #ifdef OLED_ENABLE
 
