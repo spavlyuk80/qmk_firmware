d9c4e554d816bc2ae455b674dc2924b214e825a4
Sergiy Pavlyuk <s.pavlyuk@gmail.com>
2025-01-30 00:49:18 +0100

    savng
     progress

--- keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -24,10 +24,10 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for development
   [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     , KC_ESC   ,                                        KC_CAPS , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     ,
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     , KC_TAB  ,                                         S(KC_QUOT)  , KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , 
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LSFT  ,                                        KC_QUOT   , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , 
-                    SCRL_MO  , KC_LALT  ,  KC_LCTL,    LT(1,KC_SPC)   , LT(2,_______),           KC_BSPC, LT(3,KC_ENT)   ,      _______  , _______  , KC_LGUI
+    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_QUOT  ,
+    KC_TAB  ,KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                           KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_QUOT) ,
+    KC_CAPS ,KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                           KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , _______ ,
+                    SCRL_MO  , KC_LALT  ,  KC_LCTL,   KC_LSFT,  LT(2, KC_SPC),                       LT(1,KC_ENT)  , KC_BSPC,       _______  , _______  , LT(3,KC_LGUI)
   ),
 
   [1] = LAYOUT_universal(
@@ -39,16 +39,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [2] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3   , KC_F4    , KC_F5    , KC_F6    ,                                         KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
-    SSNP_VRT ,  KC_PGUP , KC_BTN1  , KC_UP   , KC_BTN2  , KC_DEL   ,                                         KC_PGUP  , KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , KC_HOME  ,
-    SSNP_HOR ,  KC_PGDN , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_LSFT  ,                                         KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , KC_END  ,
-                  SSNP_FRE , _______ , _______  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
+    SSNP_VRT ,  KC_PGUP , KC_BTN1  , KC_UP   , KC_BTN2  , KC_DEL   ,                                         KC_PGUP  , KC_HOME ,  KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , 
+    SSNP_HOR ,  KC_PGDN , KC_LEFT  , KC_DOWN , KC_RGHT  , KC_LSFT  ,                                         KC_PGDN  , KC_END ,   KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
+                  SSNP_FRE , _______ , KC_LCTL  ,         _______  , _______  ,                   _______  , _______  , _______       , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
     RGB_TOG  , _______  , _______  , _______  , _______  , _______  ,                                        RGB_M_P  , RGB_M_B  , RGB_M_R  , RGB_M_SW , RGB_M_SN , RGB_M_K  ,
     RGB_MOD  , RGB_HUI  , RGB_SAI  , RGB_VAI  , _______  , SCRL_DVI ,                                        RGB_M_X  , RGB_M_G  , RGB_M_T  , RGB_M_TW , _______  , _______  ,
-    RGB_RMOD , RGB_HUD  , RGB_SAD  , RGB_VAD  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
-                    QK_BOOT  , KBC_RST  , _______  ,       _______  , _______     ,               _______  , _______  ,      _______  , KBC_RST  , QK_BOOT
+    CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , SCRL_DVD ,                                        CPI_D1K  , CPI_D100 , CPI_I100 , CPI_I1K  , _______  , KBC_SAVE ,
+                    QK_BOOT  , KBC_RST  , KBC_SAVE  ,       _______  , _______     ,               _______  , _______  ,      _______  , KBC_RST  , QK_BOOT
   ),
 
 
