9bcee9620810814861bb159f9fa03c76b92a042f
Sergiy Pavlyuk <s.pavlyuk@gmail.com>
2025-02-01 18:45:16 +0100

    saving progres

--- keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -27,16 +27,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_QUOT  ,
     KC_TAB  ,KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                           KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , S(KC_QUOT) ,
     KC_CAPS ,KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                           KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , _______ ,
-                    SCRL_MO  , KC_LALT  ,  KC_LCTL,   KC_LSFT,  LT(2, KC_SPC),                       LT(1,KC_ENT)  , KC_BSPC,       _______  , _______  , LT(3,KC_LGUI)
+                    SCRL_MO  , KC_LALT  ,  KC_LCTL,   KC_LSFT, LT(2, KC_SPC),                      LT(1,KC_ENT)  , KC_BSPC,  _______  , _______  , LT(3,KC_LGUI)
   ),
 
   [1] = LAYOUT_universal(
-    S(KC_8), KC_7     , KC_8    , KC_9     , S(KC_EQL)  , KC_GRV ,                                           S(KC_9)  , S(KC_0)   , KC_LBRC, KC_RBRC, S(KC_LBRC)  , S(KC_LBRC)  ,
+    S(KC_8), KC_7     , KC_8    , KC_9     , S(KC_EQL)  , KC_GRV ,                                           S(KC_9)  , S(KC_0)   , KC_LBRC, KC_RBRC, S(KC_LBRC)  , S(KC_RBRC)  ,
     KC_SLSH, KC_4     , KC_5    , KC_6     , KC_MINS  , S(KC_GRV)  ,                                         S(KC_1)  , S(KC_2)   , S(KC_3),  S(KC_4)  , S(KC_5)  , S(KC_6)  ,
     _______, KC_1     , KC_2    , KC_3     , KC_EQL, _______  ,                                              S(KC_7)  , S(KC_UNDS), KC_BSLS,S(KC_BSLS),KC_SLSH    , _______,
                   KC_0     , KC_DOT  , _______  ,         _______  , _______  ,                   _______   , _______  , _______       , _______  , _______
   ),
-
+ 
   [2] = LAYOUT_universal(
     KC_F1    , KC_F2    , KC_F3   , KC_F4    , KC_F5    , KC_F6    ,                                         KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
     SSNP_VRT ,  KC_PGUP , KC_BTN1  , KC_UP   , KC_BTN2  , KC_DEL   ,                                         KC_PGUP  , KC_HOME ,  KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , 
