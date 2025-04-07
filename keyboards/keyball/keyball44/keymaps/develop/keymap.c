4c584dcfc5a5c9b9c9bf751a46a192b24efe5f3f
Sergiy Pavlyuk <s.pavlyuk@gmail.com>
2025-04-07 23:32:45 +0200

    saving progress

--- keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -24,9 +24,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for development
   [0] = LAYOUT_universal(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                         KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_QUOT  ,
-    KC_TAB  ,KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                           KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_MINS ,
-    KC_CAPS ,KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                           KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_GRV ,
+    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     , KC_ESC   ,                                         KC_QUOT  , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , 
+    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     , KC_TAB  ,                                          KC_MINS , KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , 
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_CAPS ,                                          KC_GRV , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , 
                     SCRL_MO  , KC_LALT  ,  KC_LCTL,   LT(2, KC_SPC), KC_LSFT,                     LT(3,KC_BSPC), LT(1,KC_ENT)  ,   _______  , _______  , LT(4,KC_LGUI)
   ),
 
