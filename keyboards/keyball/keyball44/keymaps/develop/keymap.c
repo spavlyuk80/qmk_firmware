b44214c796ef179b92033e0c621800f1b3331128
Sergiy Pavlyuk <s.pavlyuk@gmail.com>
2025-04-07 23:50:40 +0200

    saving progress

--- keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -26,8 +26,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_universal(
     KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     , KC_ESC   ,                                         KC_QUOT  , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , 
     KC_A     , KC_S     , KC_D     , KC_F     , KC_G     , KC_TAB  ,                                          KC_MINS , KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , 
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_CAPS ,                                          KC_GRV , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , 
-                    SCRL_MO  , KC_LALT  ,  KC_LCTL,   LT(2, KC_SPC), KC_LSFT,                     LT(3,KC_BSPC), LT(1,KC_ENT)  ,   _______  , _______  , LT(4,KC_LGUI)
+    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LCTL ,                                          KC_GRV , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , 
+                    SCRL_MO  , KC_LALT  ,  KC_LCTL,   LT(2, KC_SPC), KC_LSFT,                     LT(3,KC_BSPC), LT(1,KC_ENT)  ,   _______  , _______  , LT(4,KC_CAPS)
   ),
 
   [1] = LAYOUT_universal(
@@ -38,10 +38,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
  
   [2] = LAYOUT_universal(
-    _______    , _______    , _______   , _______    , _______    , KC_VOLU    ,                           KC_BSPC  , KC_DEL    , _______    , _______   , _______   , _______   ,
-    SSNP_VRT ,  _______    , _______   , _______    , _______    , KC_VOLD    ,                            KC_PGUP  , KC_HOME ,  KC_BTN1  , KC_UP    , KC_BTN2  , KC_BTN3  , 
-    SSNP_HOR ,  _______    , _______   , _______    , _______    , _______    ,                            KC_PGDN  , KC_END ,   KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  ,
-                  SSNP_FRE , KC_LALT , KC_LCTL  ,   _______  , KC_LSFT  ,                           _______  , _______  , _______    , _______  , _______
+    _______    , _______    , KC_BTN1   , KC_BTN3    , KC_BTN2    , KC_VOLU    ,                            _______  , KC_PGUP ,  KC_HOME  , KC_UP    , KC_END  , _______  , 
+    SSNP_VRT ,  _______    , _______   , _______    , _______    , KC_VOLD    ,                             _______  , KC_PGDN ,   KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , 
+    SSNP_HOR ,  _______    , _______   , _______    , _______    , KC_LGUI    ,                             _______  , _______    , _______    , _______   , _______   , _______   ,
+                  SSNP_FRE , KC_LALT , KC_LCTL  ,   _______  , KC_LSFT  ,                           KC_BSPC  , KC_DEL  , _______    , _______  , _______
   ),
 
   [3] = LAYOUT_universal(
