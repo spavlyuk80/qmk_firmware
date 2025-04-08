6b7b58ccace517d4ec9dde60f82d97a7df6f85cd
Sergiy Pavlyuk <s.pavlyuk@gmail.com>
2025-04-08 11:45:44 +0200

    saving progress

--- keyboards/keyball/keyball44/keymaps/develop/keymap.c
+++ keyboards/keyball/keyball44/keymaps/develop/keymap.c
@@ -24,17 +24,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // keymap for development
   [0] = LAYOUT_universal(
-    KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     , KC_ESC   ,                                         KC_QUOT  , KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , 
-    KC_A     , KC_S     , KC_D     , KC_F     , KC_G     , KC_TAB  ,                                          KC_MINS , KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , 
-    KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     , KC_LCTL ,                                          KC_GRV , KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , 
+    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                                           KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_QUOT  ,  
+    KC_TAB  ,  KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                           KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_MINS , 
+    KC_LCTL ,  KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                           KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_GRV , 
                     SCRL_MO  , KC_LALT  ,  KC_LCTL,   LT(2, KC_SPC), KC_LSFT,                     LT(3,KC_BSPC), LT(1,KC_ENT)  ,   _______  , _______  , LT(4,KC_CAPS)
   ),
 
   [1] = LAYOUT_universal(
-    KC_1       , KC_2    , KC_3     , KC_4  , KC_5 , KC_BSLS,                                                  KC_F1    , KC_F2    , KC_F3   , KC_F4    , KC_F5    , KC_F6    ,
-     KC_6    ,    KC_7    , KC_8     , KC_9  , KC_0  , S(KC_BSLS),                                             KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
-    S(KC_2),    S(KC_EQL)  , KC_MINS    , KC_SLSH  , S(KC_8), KC_EQL  ,                                        S(KC_9)  , S(KC_0)   , KC_LBRC,  KC_RBRC,  S(KC_LBRC)   , S(KC_RBRC) ,
-                  KC_LBRC  , KC_RBRC  , KC_LCTL  ,   _______  , KC_LSFT  ,                   KC_BSPC   , _______  , _______       , _______  , _______
+    KC_BSLS,     KC_1       , KC_2    , KC_3     , KC_4  , KC_5 ,                                              KC_F1    , KC_F2    , KC_F3   , KC_F4    , KC_F5    , KC_F6    ,
+    S(KC_BSLS),  KC_6    ,    KC_7    , KC_8     , KC_9  , KC_0  ,                                             KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   , KC_F12   ,
+    S(KC_2),     S(KC_EQL)  , KC_MINS    , KC_SLSH  , S(KC_8), KC_EQL  ,                                       S(KC_9)  , S(KC_0)   , KC_LBRC,  KC_RBRC,  S(KC_LBRC)   , S(KC_RBRC) ,
+                 KC_LBRC  , KC_RBRC  , KC_LCTL  ,   _______  , KC_LSFT  ,                   KC_BSPC   , _______  , _______       , _______  , _______
   ),
  
   [2] = LAYOUT_universal(
