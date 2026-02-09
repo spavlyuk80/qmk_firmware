e409b1bf23bc682a78b11b8d1dbd1631f8ae7630
Sergiy <s.pavlyuk@gmail.com>
2026-02-09 21:58:48 +0100

    sp

--- keyboards/keyball/keyball44/keymaps/default/keymap.c
+++ keyboards/keyball/keyball44/keymaps/default/keymap.c
@@ -34,10 +34,10 @@ const key_override_t *key_overrides[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // Layer 0: Default QWERTY
   [0] = LAYOUT_right_ball(
-    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_DEL   ,
-    _______   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
-    KC_LSFT  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_EQL  ,
-               KC_LALT  , KC_LCTL  , KC_LGUI  , LT(1,KC_SPC), LT(3,KC_TAB),          KC_BSPC , LT(2,KC_ENT),                                          KC_RSFT
+    KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T     ,                            KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_MINS   ,
+    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                            KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , KC_QUOT  ,
+    LT(3, _______)  , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                            KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLSH  , KC_EQL  ,
+               KC_LALT  , KC_LCTL  , KC_LGUI  , LT(1,KC_SPC), KC_LSFT,          KC_BSPC , LT(2,KC_ENT),                                          KC_RSFT
   ),
 
   // Layer 1: Navigation and Function keys
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV   , KC_F1    , KC_F2    , KC_F3    , KC_F4    , KC_F5    ,                            KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_F11   ,
     _______  , _______  , _______  , KC_UP    , KC_ENT   , KC_DEL   ,                            KC_PGUP  , MS_BTN1  , KC_UP    , MS_BTN2  , MS_BTN3  , KC_F12   ,
     _______  , _______  , KC_LEFT  , KC_DOWN  , KC_RGHT  , KC_BSPC  ,                            KC_PGDN  , KC_LEFT  , KC_DOWN  , KC_RGHT  , _______  , _______  ,
-               _______  , _______  , _______  , _______  , _______  ,                 _______  , _______  ,                                            _______
+               _______  , _______  , _______  , _______  , _______  ,                 KC_DEL  , _______  ,                                            _______
   ),
 
   // Layer 2: Numbers and Symbols
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_GRV   , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                            KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_MINS  ,
     KC_NUBS  , KC_EXLM  , KC_AT    , KC_HASH  , KC_DLR   , KC_PERC  ,                            KC_CIRC  , KC_AMPR  , KC_ASTR  , KC_LPRN  , KC_RPRN  , KC_UNDS  ,
     S(KC_NUBS), KC_EQL  , KC_PLUS  , KC_LBRC  , KC_RBRC  , KC_BSLS  ,                            KC_LCBR  , KC_RCBR  , KC_PIPE  , _______  , _______  , _______  ,
-               _______  , _______  , _______  , _______  , _______  ,                 KC_DEL   , _______  ,                                            _______
+               _______  , _______  , _______  , _______  , _______  ,                 _______   , _______  ,                                            _______
   ),
 
   // Layer 3: Trackball settings, System, and Media
